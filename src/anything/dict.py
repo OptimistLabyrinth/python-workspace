1a83103996dd5ee4bd154d8169e93ba6eeb659d3
OptimistLabyrinth <keesung0705@gmail.com>
2024-08-07 11:45:18 +0900

    feat: can I get length of a dictionary?

---
+++ src/anything/dict.py
@@ -0,0 +1,12 @@
+def run():
+    personal_details = {
+        'key1': 'value1',
+        'key2': 'value2',
+        'key3': 'value3',
+    }
+    print(f'len(personal_details) = {len(personal_details)}')
+
+
+if __name__ == '__main__':
+    run()
+
